6beb11ec6992592e1c77b23363c7698bac1bfc87
Kanto <jekfrost555@mail.ru>
2023-04-01 20:27:22 +0300

    Поправил ошибки

--- notificationapp/src/main/java/ru/mirea/zhurin/d/r/notificationapp/MainActivity.java
+++ notificationapp/src/main/java/ru/mirea/zhurin/d/r/notificationapp/MainActivity.java
@@ -5,8 +5,12 @@ import static android.Manifest.permission.POST_NOTIFICATIONS;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -28,11 +32,28 @@ public class MainActivity extends AppCompatActivity {
             Log.d(MainActivity.class.getSimpleName().toString(), "Разрешения получены");
         } else {
             Log.d(MainActivity.class.getSimpleName().toString(), "Нет разрешений!");
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION}, PermissionCode);
+            ActivityCompat.requestPermissions(this, new String[]{POST_NOTIFICATIONS}, PermissionCode);
+
         }
     }
 
     public void onClickNotification(View view){
+        if (ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentText("Congratulation!")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setContentTitle("Mirea");
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Student Zhurin Dmitry Romanovich Notification", importance);
+        channel.setDescription("MIREA Channel");
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.createNotificationChannel(channel);
+        notificationManager.notify(1, builder.build());
 
     }
 }
